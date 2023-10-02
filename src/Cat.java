d5719f40614b4fce1e7928598410eb18bbd50025
BT_1N3_12 <raidah@nycstudents.net>
2023-10-02 11:09:53 -0400

    k

---
+++ src/Cat.java
@@ -0,0 +1,26 @@
+public class Cat {
+    //instance variables
+    private String name;
+    private int timesFed;
+
+    // constructor
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    // getter method
+    public int getTimesFed() {
+        return timesFed;
+    }
+
+    // feed the cat
+    public void feed(String food) {
+        System.out.println("Yum! " + name + " loves " + food + "!");
+        timesFed++;
+    }
+
+    // introduce the cat
+    public void introduce() {
+        System.out.println("Hello. My name is " + name);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.util.ArrayList;
+public class Student {
+    ArrayList<Double> list = new ArrayList<Double>();
+    /* Instance Variables */
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private double accumulatedTestScores;
+    private int testScoreCount;
+
+    /* Constructor; see Note 1 below */
+    public Student(String firstName, String lastName, int gradYear) {
+        accumulatedTestScores = 0.0;
+        testScoreCount = 0;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradYear = gradYear;
+    }
+
+    /* Getter Methods */
+// Returns firstName
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    // Returns lastName
+    public String getLastName() {
+
+        return lastName;
+    }
+
+    /* Setter Methods */
+// Sets gradYear to newGradYear
+    public void setGradYear(int newGradYear) {
+
+        gradYear = newGradYear;
+    }
+
+    /* All Other Methods */
+// Adds newTestScore to accumulatedTestScores
+// and increments testScoreCount by 1
+    public void addTestScore(double newTestScore) {
+        accumulatedTestScores += newTestScore;
+        testScoreCount++;
+        list.add(newTestScore);
+    }
+
+    // Returns true if the student’s average test score is greater
+// than or equal to 65; returns false otherwise (see Note 2 below)
+    public boolean isPassing() {
+        if (averageTestScore() >= 65) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Extra credit
+    public double extraCredit(int points) {
+        return accumulatedTestScores += points;
+    }
+
+    //Drop lowest grade
+    public double dropLowest() {
+        accumulatedTestScores -=
+        testScoreCount--;
+    }
+
+
+
+    // Returns the Student’s average test score as the
+// quotient of accumulatedTestScores and testScoreCount
+    public double averageTestScore(){
+        double average = accumulatedTestScores/testScoreCount;
+        return  average;
+    }
+
+        // this method prints the students full name, grad year, test average,
+// and whether they are passing (see Note 3 below)
+        public void printStudentInfo() {
+            System.out.println("Student Full Name: " + firstName + " " + lastName);
+            System.out.println("Graduation Year: " + gradYear);
+            System.out.println("Number of tests: " + testScoreCount);
+            System.out.println("Average Test Score: " + averageTestScore());
+            System.out.println("Student is passing: " + isPassing());
+        }
+}
